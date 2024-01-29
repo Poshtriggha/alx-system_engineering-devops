80fe48271ea1b49bfccb8f91a6d1bb6166888166
posh <mrposhboo@gmail.com>
2024-01-29 14:01:26 -0800

    employee

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+Write a Python script that, using this REST API, for a given employee ID, 
+returns information about his/her TODO list progress.
 
 Requirements:
 
 You must use urllib or requests module
 The script must accept an integer as a parameter, which is the employee ID
-The script must display on the standard output the employee TODO list progress in this exact format:
-First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+The script must display on the standard output the employee TODO list progress 
+in this exact format:
+
+First line: Employee EMPLOYEE_NAME is done with 
+tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+
 EMPLOYEE_NAME: name of the employee
 NUMBER_OF_DONE_TASKS: number of completed tasks
-TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
-Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+TOTAL_NUMBER_OF_TASKS: total number of tasks, 
+which is the sum of completed and non-completed tasks
+
+Second and N next lines display the title of completed tasks: 
+TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
 """
 
 import requests
@@ -32,7 +40,7 @@ if __name__ == "__main__":
 
     todos = todos_response.json()
 
-    completed = []  
+    completed = []
 
     for todo in todos:
         if todo.get("completed") is True:
@@ -41,5 +49,5 @@ if __name__ == "__main__":
     print("Employee {} is done with tasks({}/{})".format(user.get("name"), len(completed),
                                                           len(completed), len(todos)))
 
-    for task in completed:  
+    for task in completed:
         print("\t {}".format(task))
