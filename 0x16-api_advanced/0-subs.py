5b1cf5aef350ba4263b36ba6e873df5e5d39af1e
posh <mrposhboo@gmail.com>
2024-02-16 19:35:02 +0000

    kij

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -12,11 +13,4 @@ def number_of_subscribers(subreddit):
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
-    
-    # Add a return statement to return the number of subscribers
-    return results.get("subscribers", 0)
-
-# Example usage:
-subreddit_name = "python"
-subscribers_count = number_of_subscribers(subreddit_name)
-print(f"The subreddit '{subreddit_name}' has {subscribers_count} subscribers.")
+    return results.get("subscribers")
